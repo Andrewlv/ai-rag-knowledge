7eca3af68dd8e355d430b0f35c6a3a81ff8cfa83
Andrewlv <2289380832@qq.com>
2025-03-20 15:58:14 +0800

    feat: Ollama RAG 知识库接口服务实现

---
+++ andrew-dev-tech-api/src/main/java/com/andrew/dev/tech/api/IRAGService.java
@@ -0,0 +1,12 @@
+package com.andrew.dev.tech.api;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface IRAGService {
+
+    Response<List<String>> queryRagTagList();
+
+    Response<String> uploadFile(String ragTag, List<MultipartFile> files);
+}
---
+++ andrew-dev-tech-trigger/src/main/java/com/andrew/dev/tech/trigger/http/RAGController.java
@@ -0,0 +1,79 @@
+package com.andrew.dev.tech.trigger.http;
+
+import com.andrew.dev.tech.api.IRAGService;
+import com.andrew.dev.tech.api.Response;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RList;
+import org.redisson.api.RedissonClient;
+import org.springframework.ai.chat.ChatClient;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.ollama.OllamaChatClient;
+import org.springframework.ai.reader.tika.TikaDocumentReader;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.PgVectorStore;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api/v1/rag/")
+public class RAGController implements IRAGService {
+
+
+    @Resource
+    private OllamaChatClient ollamaChatClient;
+
+    @Resource
+    private TokenTextSplitter tokenTextSplitter;
+
+    @Resource
+    private SimpleVectorStore simpleVectorStore;
+
+    @Resource
+    private PgVectorStore pgVectorStore;
+
+    @Resource
+    private ChatClient chatClient;
+
+    @Resource
+    private RedissonClient redissonClient;
+
+    @RequestMapping(value = "query_rag_tag_list", method = RequestMethod.GET)
+    @Override
+    public Response<List<String>> queryRagTagList() {
+        RList<String> elements = redissonClient.getList("ragTag");
+        return Response.<List<String>>builder()
+                .code("0000")
+                .info("调用成功")
+                .data(elements)
+                .build();
+    }
+
+    @RequestMapping(value = "file/upload", method = RequestMethod.POST, headers = "content-type=multipart/form-data")
+    @Override
+    public Response<String> uploadFile(@RequestParam String ragTag, @RequestParam List<MultipartFile> files) {
+        log.info("上传知识库开始 {}", ragTag);
+        for (MultipartFile file : files) {
+            TikaDocumentReader documentReader = new TikaDocumentReader(file.getResource());
+            List<Document> documents = documentReader.get();
+            List<Document> documentSplitterList = tokenTextSplitter.apply(documents);
+
+            documents.forEach(document -> document.getMetadata().put("knowledge", ragTag));
+            documentSplitterList.forEach(document -> document.getMetadata().put("knowledge", ragTag));
+
+            pgVectorStore.accept(documentSplitterList);
+
+            RList<Object> elements = redissonClient.getList("ragTag");
+            if (!elements.contains(ragTag)) {
+                elements.add(ragTag);
+            }
+        }
+        log.info("上传知识库结束 {}", ragTag);
+        return Response.<String>builder().code("0000").info("调用成功").build();
+    }
+}
